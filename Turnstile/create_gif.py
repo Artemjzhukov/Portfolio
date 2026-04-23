c33adfdc3f85b7d88c4461fd9c143459195444aa
Artemjzhukov <artemjzhukov@gmail.com>
2026-04-23 20:13:52 +0400

    save

--- Turnstile/create_gif.py
+++ Turnstile/create_gif.py
@@ -3,13 +3,15 @@ from pathlib import Path
 
 # Укажите пути к вашим трём фото (используйте сырые строки r"..." или двойные слеши)
 image_paths = [
-    r"C:\Users\mamon\Downloads\photo_2026-04-23_15-46-08.jpg",   # замените на реальный путь
+    r"C:\Users\mamon\Downloads\photo_2026-04-23_15-46-20.jpg",   # замените на реальный путь
     r"C:\Users\mamon\Downloads\photo_2026-04-23_15-46-15.jpg",
-    r"C:\Users\mamon\Downloads\photo_2026-04-23_15-46-20.jpg",
+    r"C:\Users\mamon\Downloads\photo_2026-04-23_15-46-08.jpg",
 ]
 
 # Папка и имя для сохранения GIF
 output_path = r"C:\Users\mamon\Downloads\output.gif"
+
+# python create_gif.py
 
 # Настройки GIF
 duration_per_image_ms = 1000   # сколько миллисекунд показывать каждый кадр (1000 = 1 сек)
