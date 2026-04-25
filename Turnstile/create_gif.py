268c464a36be683ef2b61122a09ac3828dd8412b
Artemjzhukov <artemjzhukov@gmail.com>
2026-04-25 14:01:30 +0400

    4

--- Turnstile/create_gif.py
+++ Turnstile/create_gif.py
@@ -1,11 +1,12 @@
 import imageio.v3 as iio
 from pathlib import Path
 
-# Укажите пути к вашим трём фото (используйте сырые строки r"..." или двойные слеши)
+# Укажите пути к вашим фото (используйте сырые строки r"..." или двойные слеши)
 image_paths = [
     r"C:\Users\mamon\Downloads\photo_2026-04-23_15-46-20.jpg",   # замените на реальный путь
     r"C:\Users\mamon\Downloads\photo_2026-04-23_15-46-15.jpg",
     r"C:\Users\mamon\Downloads\photo_2026-04-23_15-46-08.jpg",
+    r"C:\Users\mamon\Downloads\photo_2026-04-24_12-57-52.jpg"
 ]
 
 # Папка и имя для сохранения GIF
